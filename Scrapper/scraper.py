f9bcbe972721ef7b345b903a23d4c44a63fd4fc0
nada-mah <cutenada.lara4@gmail.com>
2024-05-04 20:35:53 +0300

    add documentation to functions

--- Scrapper/scraper.py
+++ Scrapper/scraper.py
@@ -7,10 +7,17 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+from os import environ
 
 def get_profile(Account_list): 
     """
+    Scrapes the profiles of the given Twitter accounts and returns the HTML content.
     
+    Args:
+        Account_list: List of Twitter account URLs.
+
+    Returns:
+        list: List of HTML content of the profiles.
     """
     resp = []
     options = ChromeOptions()
@@ -27,11 +34,11 @@ def get_profile(Account_list):
         if x:
             x.click()    
             username = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[autocomplete="username"]')))
-            username.send_keys("")
+            username.send_keys(environ.get("USERNAME"))
             username.send_keys(Keys.ENTER)
 
             password = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="password"]')))
-            password.send_keys("")
+            password.send_keys(environ.get("PASSWORD"))
             password.send_keys(Keys.ENTER)
             time.sleep(5)   
         driver.execute_script("window.scrollTo(0, 1000);")
@@ -41,7 +48,16 @@ def get_profile(Account_list):
     return resp
 
 def get_data(Account_list,Ticker,Interval):
-    x=0
+    """
+    Scrapes the given Twitter accounts html for the number of mentions of a stock symbol.
+
+    Args:
+        Account_list (list): List of Twitter account URLs.
+        Ticker (str): Stock symbol to search for (e.g., "$TSLA").
+        Interval (int): Time interval in minutes.
+        
+    """
+    NumberOfMentions=0
     if Account_list:
         resp = get_profile(Account_list)
         for profile in resp:
@@ -53,6 +69,6 @@ def get_data(Account_list,Ticker,Interval):
                 tweets = None
 
             for tweet in tweets:
-                x += tweet.count(Ticker)
+                NumberOfMentions += tweet.count(Ticker)
 
-        print(f'{Ticker} was mentioned {x} times in the last {Interval} minutes.”')
+        print(f'{Ticker} was mentioned {NumberOfMentions} times in the last {Interval} minutes.”')
